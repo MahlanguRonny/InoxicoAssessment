58c3f6808df4c0397c82ad153c833f0d4553ef04
aaronm <RonnyM@automationexchange.co.za>
2022-04-21 23:20:52 +0200

    Added words into new list and returned the largest(max)

--- Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Inoxico.TechnicalQuestions.Answers
 {
@@ -8,15 +9,21 @@ namespace Inoxico.TechnicalQuestions.Answers
     {
         public static int GetLongestSentance(string s)
         {
+            //split the sentences using the given delimeters
             string[] sentences = s.Split(new Char[] { '!', '.', '?' });
             List<string> finalWordList = new List<string>();
 
-            foreach(var sentence in sentences)
+            foreach (var sentence in sentences)
             {
-                //var words = sentence.Split(' ');
+                // split words on each sentence using a space
+                var words = sentence.Split(' ');
+                foreach (var word in words)
+                {
+                    finalWordList.Add(word);
+                }
             }
 
-            return 0;
+            return finalWordList.OrderByDescending(x => x.Length).First().Length;
         }
     }
 }
