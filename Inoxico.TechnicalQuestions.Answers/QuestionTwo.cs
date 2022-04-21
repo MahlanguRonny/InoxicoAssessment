07902479ee74110422af204d9080570fe87ab618
aaronm <RonnyM@automationexchange.co.za>
2022-04-22 00:06:30 +0200

    Code clean up and addition of comments

--- Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
@@ -10,10 +10,13 @@ namespace Inoxico.TechnicalQuestions.Answers
         {
             List<int> sums = new List<int>();
             int i = 0;
+            // from the provided array group them into groups of 3
             var triplets = from x in points
                            let num = i++
                            group x by num / 3 into g
                            select g.ToArray();
+
+            //loop through each item(grpuped intengers) and calculate the sum there of and add to a list.
             foreach (var item in triplets)
             {
                 sums.Add(item.Sum());
